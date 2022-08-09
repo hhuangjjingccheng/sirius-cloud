da2a16eedbe58b98810ad66ba74130ccd79add9d
HJC <1431413982@qq.com>
2022-08-09 17:48:31 +0800

    1.使用Redisson实现锁

---
+++ sirius-demo/src/main/java/com/sirius/demo/config/RedissonConnectionConfig.java
@@ -0,0 +1,41 @@
+package com.sirius.demo.config;
+
+import org.redisson.Redisson;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
+
+/**
+ * @author huangjingcheng
+ * @version 1.0.0
+ * @ClassName RedissonConfig.java
+ * @Description TODO
+ * @createTime 2022年08月09日 17:16:00
+ */
+@Configuration
+public class RedissonConnectionConfig {
+
+    private static final String REDIS = "redis://";
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private String redisPort;
+
+    @Value("${spring.redis.database}")
+    private Integer redisDataBase;
+
+
+    @Bean
+    public Redisson redisson(){
+        Config config = new Config();
+        // 单机模式
+        config.useSingleServer().setAddress(REDIS+redisHost+":"+redisPort).setDatabase(redisDataBase);
+        return (Redisson) Redisson.create(config);
+    }
+
+}
--- sirius-demo/src/main/java/com/sirius/demo/redis/controller/RedisDemoController.java
+++ sirius-demo/src/main/java/com/sirius/demo/redis/controller/RedisDemoController.java
@@ -1,12 +1,15 @@
 package com.sirius.demo.redis.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.redisson.Redisson;
+import org.redisson.api.RLock;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author huangjingcheng
@@ -23,21 +26,27 @@ public class RedisDemoController {
     @Resource
     private StringRedisTemplate stringRedisTemplate;
 
+    @Resource
+    private Redisson redisson;
 
-
-    @RequestMapping(value = "/duductStock",method = RequestMethod.POST)
-    public String duductStock(){
-        synchronized (this){
+    @RequestMapping(value = "/duductStock", method = RequestMethod.POST)
+    public String duductStock() {
+        String lockName = "lockName";
+        RLock lock = redisson.getLock(lockName);
+        try {
+            lock.lock(30, TimeUnit.SECONDS);
             int stock = Integer.parseInt(stringRedisTemplate.opsForValue().get("stock"));
-            if (stock>0){
-                int result = stock-1;
-                stringRedisTemplate.opsForValue().set("stock",result+"");
-                log.info("扣减成功，库存剩余："+result+"");
-            }else {
+            if (stock > 0) {
+                int result = stock - 1;
+                stringRedisTemplate.opsForValue().set("stock", result + "");
+                log.info("扣减成功，库存剩余：" + result + "");
+            } else {
                 log.info("扣减失败，库存不足");
             }
-            return "end";
+        } finally {
+            lock.unlock();
         }
 
+        return "end";
     }
 }
