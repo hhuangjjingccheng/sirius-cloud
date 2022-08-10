bf7643b6acba53cff3b944b94b8eb515cfe108dc
huangjingcheng <1431413982@qq.com>
2022-08-11 00:08:29 +0800

    一.代码格式化

--- sirius-demo/src/test/java/com/sirius/demo/thread/CollectionThreadSecurity.java
+++ sirius-demo/src/test/java/com/sirius/demo/thread/CollectionThreadSecurity.java
@@ -15,21 +15,22 @@ public class CollectionThreadSecurity {
 
     /**
      * ArrayList线程不安全
+     *
      * @throws InterruptedException
      */
     @Test
     public void demoOne() throws InterruptedException {
         ArrayList<Thread> threads = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        for (int i = 0;i<1000;i++ ){
+        for (int i = 0; i < 1000; i++) {
             int finalI = i;
-            Thread t = new Thread(()->{
-               list.add(finalI);
+            Thread t = new Thread(() -> {
+                list.add(finalI);
             });
             t.start();
             threads.add(t);
         }
-        for (Thread j:threads){
+        for (Thread j : threads) {
             j.join();
         }
         System.out.println("结束");
@@ -38,24 +39,25 @@ public class CollectionThreadSecurity {
 
     /**
      * CopyOnWriteArrayList
-     *
+     * <p>
      * ReentrantLock 使得
      * ArrayList线程安全
+     *
      * @throws InterruptedException
      */
     @Test
     public void demoTwo() throws InterruptedException {
         ArrayList<Thread> threads = new ArrayList<>();
         List<Integer> list = new CopyOnWriteArrayList<>();
-        for (int i = 0;i<1000;i++ ){
+        for (int i = 0; i < 1000; i++) {
             int finalI = i;
-            Thread t = new Thread(()->{
+            Thread t = new Thread(() -> {
                 list.add(finalI);
             });
             t.start();
             threads.add(t);
         }
-        for (Thread j:threads){
+        for (Thread j : threads) {
             j.join();
         }
         System.out.println("结束");
@@ -64,24 +66,25 @@ public class CollectionThreadSecurity {
 
     /**
      * Vector
-     *
+     * <p>
      * synchronized 使得
      * ArrayList 线程安全
+     *
      * @throws InterruptedException
      */
     @Test
     public void demoThree() throws InterruptedException {
         ArrayList<Thread> threads = new ArrayList<>();
         List<Integer> list = new Vector<>();
-        for (int i = 0;i<1000;i++ ){
+        for (int i = 0; i < 1000; i++) {
             int finalI = i;
-            Thread t = new Thread(()->{
+            Thread t = new Thread(() -> {
                 list.add(finalI);
             });
             t.start();
             threads.add(t);
         }
-        for (Thread j:threads){
+        for (Thread j : threads) {
             j.join();
         }
         System.out.println("结束");
@@ -90,24 +93,25 @@ public class CollectionThreadSecurity {
 
     /**
      * Collections.synchronizedList
-     *
+     * <p>
      * synchronized 使得
      * ArrayList线程安全
+     *
      * @throws InterruptedException
      */
     @Test
     public void demoFour() throws InterruptedException {
         ArrayList<Thread> threads = new ArrayList<>();
         List<Integer> list = Collections.synchronizedList(new ArrayList<>());
-        for (int i = 0;i<1000;i++ ){
+        for (int i = 0; i < 1000; i++) {
             int finalI = i;
-            Thread t = new Thread(()->{
+            Thread t = new Thread(() -> {
                 list.add(finalI);
             });
             t.start();
             threads.add(t);
         }
-        for (Thread j:threads){
+        for (Thread j : threads) {
             j.join();
         }
         System.out.println("结束");
@@ -116,21 +120,22 @@ public class CollectionThreadSecurity {
 
     /**
      * HashMap 线程不安全
+     *
      * @throws InterruptedException
      */
     @Test
     public void demoFive() throws InterruptedException {
         ArrayList<Thread> threads = new ArrayList<>();
-        Map<String,Object> map = new HashMap<>();
-        for (int i = 0;i<10000;i++ ){
+        Map<String, Object> map = new HashMap<>();
+        for (int i = 0; i < 10000; i++) {
             int finalI = i;
-            Thread t = new Thread(()->{
-                map.put(finalI+"",finalI);
+            Thread t = new Thread(() -> {
+                map.put(finalI + "", finalI);
             });
             t.start();
             threads.add(t);
         }
-        for (Thread j:threads){
+        for (Thread j : threads) {
             j.join();
         }
         System.out.println("结束");
@@ -139,24 +144,25 @@ public class CollectionThreadSecurity {
 
     /**
      * ConcurrentHashMap
-     *
+     * <p>
      * synchronized 使得
      * HashMap 线程安全
+     *
      * @throws InterruptedException
      */
     @Test
     public void demoSix() throws InterruptedException {
         ArrayList<Thread> threads = new ArrayList<>();
-        Map<String,Object> map = new ConcurrentHashMap<>();
-        for (int i = 0;i<10000;i++ ){
+        Map<String, Object> map = new ConcurrentHashMap<>();
+        for (int i = 0; i < 10000; i++) {
             int finalI = i;
-            Thread t = new Thread(()->{
-                map.put(finalI+"",finalI);
+            Thread t = new Thread(() -> {
+                map.put(finalI + "", finalI);
             });
             t.start();
             threads.add(t);
         }
-        for (Thread j:threads){
+        for (Thread j : threads) {
             j.join();
         }
         System.out.println("结束");
@@ -165,24 +171,25 @@ public class CollectionThreadSecurity {
 
     /**
      * Hashtable
-     *
+     * <p>
      * synchronized 使得
      * HashMap 线程安全
+     *
      * @throws InterruptedException
      */
     @Test
     public void demoSeven() throws InterruptedException {
         ArrayList<Thread> threads = new ArrayList<>();
-        Map<String,Object> map = new Hashtable<>();
-        for (int i = 0;i<10000;i++ ){
+        Map<String, Object> map = new Hashtable<>();
+        for (int i = 0; i < 10000; i++) {
             int finalI = i;
-            Thread t = new Thread(()->{
-                map.put(finalI+"",finalI);
+            Thread t = new Thread(() -> {
+                map.put(finalI + "", finalI);
             });
             t.start();
             threads.add(t);
         }
-        for (Thread j:threads){
+        for (Thread j : threads) {
             j.join();
         }
         System.out.println("结束");
@@ -191,48 +198,49 @@ public class CollectionThreadSecurity {
 
     /**
      * HashSet 线程不安全
+     *
      * @throws InterruptedException
      */
     @Test
     public void demoEight() throws InterruptedException {
         ArrayList<Thread> threads = new ArrayList<>();
         Set<String> set = new HashSet<>();
-        for (int i = 0;i<10000;i++ ){
+        for (int i = 0; i < 10000; i++) {
             int finalI = i;
-            Thread t = new Thread(()->{
-                set.add(finalI+"");
+            Thread t = new Thread(() -> {
+                set.add(finalI + "");
             });
             t.start();
             threads.add(t);
         }
-        for (Thread j:threads){
+        for (Thread j : threads) {
             j.join();
         }
         System.out.println("结束");
         System.out.println(set.size());
     }
+
     /**
      * CopyOnWriteArraySet
-     *
+     * <p>
      * ReentrantLock 使得
      * HashSet 线程安全
-     * @throws InterruptedException
      *
-     * 其他方式使得   HashSet 线程安全    ：1、Collections.synchronizedSet(new HashSet());
+     * @throws InterruptedException 其他方式使得   HashSet 线程安全    ：1、Collections.synchronizedSet(new HashSet());
      */
     @Test
     public void demoNine() throws InterruptedException {
         ArrayList<Thread> threads = new ArrayList<>();
         Set<String> set = new CopyOnWriteArraySet<>();
-        for (int i = 0;i<10000;i++ ){
+        for (int i = 0; i < 10000; i++) {
             int finalI = i;
-            Thread t = new Thread(()->{
-                set.add(finalI+"");
+            Thread t = new Thread(() -> {
+                set.add(finalI + "");
             });
             t.start();
             threads.add(t);
         }
-        for (Thread j:threads){
+        for (Thread j : threads) {
             j.join();
         }
         System.out.println("结束");
