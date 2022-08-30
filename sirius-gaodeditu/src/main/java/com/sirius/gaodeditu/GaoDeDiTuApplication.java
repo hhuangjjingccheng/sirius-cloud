331b1a14fb304e6345ecaa94fb79db1b7efbb67b
huangjingcheng <hhuangjjingccheng@outlook.com>
2022-08-30 14:49:38 +0800

    1.将sirius-gaodeditu服务注册到nacos服务端

--- sirius-gaodeditu/src/main/java/com/sirius/gaodeditu/GaoDeDiTuApplication.java
+++ sirius-gaodeditu/src/main/java/com/sirius/gaodeditu/GaoDeDiTuApplication.java
@@ -3,8 +3,10 @@ package com.sirius.gaodeditu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
 public class GaoDeDiTuApplication {
     public static void main(String[] args) {
         SpringApplication.run(GaoDeDiTuApplication.class,args);
